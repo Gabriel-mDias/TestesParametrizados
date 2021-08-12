7985f425fabc34b02391ce673e2e8509a9260008
Gabriel-mDias <gabrielmagdias@gmail.com>
2021-08-12 00:08:32 -0300

    Análise Valor Limite no teste de Fibonacci

--- TestesParametrizados/src/test/java/FibonacciTest.java
+++ TestesParametrizados/src/test/java/FibonacciTest.java
@@ -10,10 +10,17 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class FibonacciTest {
 
+    /**
+     * Análise de valor limite para fibonacci tem o problema ao definir seu limite, já que engloba todos os números naturais. <br>
+     * Além disso, devemos trabalhar com valores suportados pela tipagem definida.<br>
+     * Então criamos um teto com o valor de 40, assim os testes se darão para o intervalo [0,40] visando os vizinhos de ambos os limites.
+     * @return 
+     */
     @Parameters(name = "{index}: fib({0})={1}")
     public static Iterable<Object[]> data() {
         return Arrays.asList(new Object[][] { 
-                 { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 }, { 4, 3 }, { 5, 5 }, { 6, 8 }
+            { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 }, { 4, 3 }, { 5, 5 },
+            {38, 39088169},{39, 63245986},{40,102334155},{41,165580141},{42,267914296}
            });
     }
 
@@ -30,5 +37,4 @@ public class FibonacciTest {
         assertEquals(expected, Fibonacci.compute(input));
     }
     
-  
 }
